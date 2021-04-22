f0504b02af07706e67de366fcd2a6fbbf4323747
anoopkjacob <9745360897akj@gmail.com>
2021-04-23 00:22:59 +0530

    donughtchart

--- Router/supplierRoutes.js
+++ Router/supplierRoutes.js
@@ -14,7 +14,7 @@ router.post("/requestadd", async (req, resp) => {
         {
           supplierid=req.body.supplier
         }
-        RequestTemplateCopy.findOne({ "supplierid":supplierid,"productid":req.body.productid})
+        RequestTemplateCopy.findOne({ "supplierid":supplierid,"productid":req.body.productid,"status":"pending"})
           .exec((err,reqtabledata)=>{
             if(err){
               resp.json( {message : "bill error "});
@@ -94,5 +94,115 @@ router.post("/requestadd", async (req, resp) => {
           .json({ error: error, message: "Error updating" });
       }
     });
+
+ router.post("/requestgetwithid", async (req, resp) => {
+      try{
+        RequestTemplateCopy.find({supplierid:req.body.supplierid}).sort({date: -1})
+      .exec((err,requestaddata)=>{
+         if(err){
+          resp.json( {message : "No request"});
+         }else{
+             resp.json(requestaddata);
+         }
+      });
+      }
+      catch(error){
+          return resp
+          .status(400)
+          .json({ error: err, message: "Error fetching data" });
+      }
+    });
+
+
+  router.put("/ACCEPT", async (req, resp) => {
+      try {
+          console.log(req.body)
+          const query = { "_id":req.body.id}; 
+          const update = {
+            "$set": {
+              "status":"Accept"
+            }
+          };
+          // Return the updated document instead of the original document
+          const options = { returnNewDocument: true };
+          return RequestTemplateCopy.findOneAndUpdate(query, update, options)
+            .then(updatedDocument1 => {
+              if(updatedDocument1) {
+                resp.status(200).json({ message: "Accepted"});
+    
+                console.log(`Successfully Accepted ${updatedDocument1}.`)
+              } else {
+                resp.status(200).json({ message: "Accepted failed"});
+                console.log("Accepted failed")
+              }
+              return updatedDocument1
+            })
+            .catch(err => console.error(`Failed to find and update document: ${err}`))
+          
+      } catch (error) {
+        return resp
+          .status(400)
+          .json({ error: error, message: "Error updating" });
+      }
+    });
+
+    
+router.put("/REJECT", async (req, resp) => {
+  try {
+    console.log(req.body)
+    const query = { "_id":req.body.id};
+    const update = {
+      "$set": {
+        "status":"Reject"
+      }
+    };
+    // Return the updated document instead of the original document
+    const options = { returnNewDocument: true };
+    return RequestTemplateCopy.findOneAndUpdate(query, update, options)
+      .then(updatedDocument1 => {
+        if(updatedDocument1) {
+          resp.status(200).json({ message: "Reject"});
+
+          console.log(`Successfully Reject ${updatedDocument1}.`)
+        } else {
+          resp.status(200).json({ message: "Reject failed"});
+          console.log("Reject failed")
+        }
+        return updatedDocument1
+      })
+      .catch(err => console.error(`Failed to find and update document: ${err}`))    
+  } catch (error) {
+    return resp
+      .status(400)
+      .json({ error: error, message: "Error updating" });
+  }
+});
+
+router.get("/totalcount", async (req, resp) => {
+  try{
+    RequestTemplateCopy.aggregate([
+      {
+      $group:{
+        _id:{status:"$status"},
+        count:{$sum:1}
+      }
+    }
+    ])
+  .exec((err,requestddata)=>{
+     if(err){
+      resp.json( {message : "no request"});
+     }else{
+        
+         resp.json(requestddata);
+     }
+  });
+  }
+  catch(error){
+      return resp
+      .status(400)
+      .json({ error: err, message: "Error fetching data" });
+  }
+}); 
+
 
 module.exports = router;
